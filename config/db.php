df56361623f0194f3e0784fbc4e5621f283c4edf
sdeix <you@example.com>
2024-03-16 23:06:40 +0300

    Использование illuminate/database и illuminate/events

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+   'driver' => 'mysql',
+   'host' => 'db',
+   'database' => 'db',
+   'username' => 'root',
+   'password' => 'qwe',
+   'charset' => 'utf8',
+   'collation' => 'utf8_unicode_ci',
+   'prefix' => '',
+];
