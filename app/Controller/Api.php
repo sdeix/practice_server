d0c271349c93922adad0ecde0ce9e89b99cbb841
sdeix <you@example.com>
2024-03-21 22:30:13 +0300

    добавлен функционал по api

--- app/Controller/Api.php
+++ app/Controller/Api.php
@@ -1,35 +1,74 @@
 <?php
 
 namespace Controller;
+
 use Src\Auth\Auth;
 use Model\Subdivision;
 use Src\Request;
 use Src\View;
 use Model\User;
+use Model\Abonent;
+
+
 class Api
 {
-   public function index(): void
-   {
-       $posts = Subdivision::all()->toArray();
+    public function index(): void
+    {
+        $posts = Subdivision::all()->toArray();
 
-       (new View())->toJSON($posts);
-   }
-
-   public function login(Request $request): void
-   {
-    if (Auth::attempt($request->all())) {
-        $user = User::where('login', $request->login)->get();
-        (new View())->toJSON([$user[0]->token]);
+        (new View())->toJSON($posts);
     }
-        
-       (new View())->toJSON(["Не удалось авторизироваться"]);
-   }
-   public function rew(Request $request): void
-   {
-    $headers = trim($_SERVER["HTTP_BEARER"]);
-       (new View())->toJSON([$headers]);
-   }
 
+    public function login(Request $request): void
+    {
+        if (Auth::attempt($request->all())) {
+            $user = User::where('login', $request->login)->get();
+            (new View())->toJSON([$user[0]->token]);
+        }
+
+        (new View())->toJSON(["Не удалось авторизироваться"]);
+    }
+    public function subdivisions(Request $request): void
+    {
+        $header = trim($_SERVER["HTTP_BEARER"]);
+        if (count(User::where('token', $header)->get()) != 0) {
+            (new View())->toJSON(Subdivision::all()->toArray());
+        }
+
+        (new View())->toJSON(["Недействительный токен"]);
+    }
+    public function abonents(Request $request): void
+    {
+        $header = trim($_SERVER["HTTP_BEARER"]);
+        if (count(User::where('token', $header)->get()) != 0) {
+            (new View())->toJSON(Abonent::all()->toArray());
+        }
+        (new View())->toJSON(["Недействительный токен"]);
+    }
+
+    public function addabonent(Request $request): void
+    {
+
+        $header = trim($_SERVER["HTTP_BEARER"]);
+        if (count(User::where('token', $header)->get()) != 0) {
+            if ($request->name && $request->surname && $request->patronymic && $request->dateofbirth && $request->subdivision) {
+                if (count(Subdivision::where('subdivisionname', $request->subdivision)->get()) != 0) {
+                    if(Abonent::create(['name' => $request->name, 'surname' => $request->surname, 'patronymic' => $request->patronymic, 'dateofbirth' => $request->dateofbirth, 'subdivision' => $request->subdivision])){
+                        (new View())->toJSON(["Абонент создан"]);
+                    }
+
+                } else {
+                    (new View())->toJSON(["Недействительное название подразделения, посмотреть существующие можно по пдресу /api/subdivisions"]);
+                }
+            }
+            else{
+                (new View())->toJSON(["Не заполнены все данные"]);    
+            }
+
+
+        }
+        (new View())->toJSON(["Недействительный токен"]);
+    }
 
 }
 
--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,7 @@ use Src\Route;
 
 Route::add('GET', '/', [Controller\Api::class, 'index']);
 Route::add('POST', '/login', [Controller\Api::class, 'login']);
-Route::add('POST', '/rew', [Controller\Api::class, 'rew']);
+Route::add('GET', '/subdivisions', [Controller\Api::class, 'subdivisions']);
+Route::add('GET', '/abonents', [Controller\Api::class, 'abonents']);
+Route::add('POST', '/addabonent', [Controller\Api::class, 'addabonent']);
 
